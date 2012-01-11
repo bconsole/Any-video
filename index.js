a99f5db86030f4254ebefa616bcd195ad5a6fd42
tyrone-mac <beatconsole@googlemail.com>
2012-01-11 00:18:10 +0000

    added a exclamation mark to index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, {
     	'Content-Type': 'text/plain'
 	});
-	res.end('Hello Tyrone\n');
+	res.end('Hello Tyrone!\n');
 });
 server.listen(process.env.PORT, '0.0.0.0'); 
