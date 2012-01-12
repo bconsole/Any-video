8b6ff9f1738de92ec45cd018a3f8f0609055bddc
bconsole <beatconsole@googlemail.com>
2012-01-12 14:58:32 +0000

    added this from my work pc

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, {
     	'Content-Type': 'text/plain'
 	});
-	res.end('Hello Tyrone! Just adding some extra stuff for git\n');
+	res.end('Hello Tyrone! Just adding some extra stuff for git, and some more stuff on windows pc\n');
 });
 server.listen(process.env.PORT, '0.0.0.0'); 
