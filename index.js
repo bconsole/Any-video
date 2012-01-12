de66a242106a12c65d79ecd312f017c39694fbf7
bconsole <beatconsole@googlemail.com>
2012-01-12 00:22:11 +0000

    trying from linux box

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, {
     	'Content-Type': 'text/plain'
 	});
-	res.end('Hello Tyrone!\n');
+	res.end('Hello Tyrone! Just adding some extra stuff for git\n');
 });
 server.listen(process.env.PORT, '0.0.0.0'); 
