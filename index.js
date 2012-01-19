830a20bcfbab5691832283679e117ef20d215629
bconsole <beatconsole@googlemail.com>
2012-01-19 12:45:57 +0000

    Add old logic back to the index page

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-var http  = require('http');
-var server = http.createServer(function (req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'	
-	});	
-	res.end('Hello Tyrone\n');
+var express = require('express');
+var app 	= express.createServer();
+var video = require('./lib/video/video');
+// var util 	= require('util');
+// var fs 		= require('fs');
+
+app.get('/', function(req, res){
+	res.header('Content-Type', 'text/html');	
+	res.write('');
+
+	video.load(1, function(err, vid) {
+		res.write(vid.url);
+		res.end();
+	});
 });
-server.listen(process.env.PORT, '0.0.0.0'); 
+
+// app.get('/pump', function(req, res){
+// 	res.header('Content-Type', 'text/html');	
+// 	var rs = fs.createReadStream(__dirname + '/public/html_templates/index.html');
+// 	console.log(__dirname + '/public/html_templates/index.html');
+// 	util.pump(rs, res);
+// 	res.end('hello');
+// });
+
+app.listen(1979);
+
+console.log('application running on http://localhost:1979/');
