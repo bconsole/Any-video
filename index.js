a6e7072f2d1bfab44ca6b934c43eb3cb0e44f9a1
bconsole <beatconsole@googlemail.com>
2012-01-25 23:44:53 +0000

    refactoring and added css and js to browser page

--- index.js
+++ index.js
@@ -6,106 +6,106 @@ var rootDir = __dirname;
 var util 	= require('util');
 var fs 		= require('fs');
 
-app.get('/', function(req, res){
-	res.header('Content-Type', 'text/html');	
-	res.write('<head>');
-	res.write('<title>Watcha Watching</title>');
-	res.write('<link href="/css" type="text/css" rel="stylesheet" />');
-	res.write('</head>');
-	res.write('<h1>Watcha Watching</h1>');
-	res.write('<h2>' + rootDir + '</h2>');
+function page_load(req, res, id) {
+	getTemplate(res, function(err) {
+		if (err) {
+			throw err;
+		}
 
+		video.load(id, function(err, vid) {
+			videoEmbedHtml.getEmbedHtml(vid, function(err, embedHtml) {
+				if (err) {
+					throw err;
+				}
+
+				res.write('<div id="content">' + embedHtml + '<br clear="all" /></div>');
+				res.end();					
+			});
+		});
+	});	
+}
+
+function getTemplate(res, callback){	
+	loadHead(function(err, head) {
+		if (err) {
+			throw err;
+		}
+		res.header('Content-Type', 'text/html');	
+		res.write(head);
+		//res.write('<h1>Watcha Watching</h1>');
+
+		loadNav(function(err, nav){
+			res.write(nav);
+
+			callback(null)
+		});	
+	});
+}
+
+function loadHead(callback) {
+	var h = '';
+	h += '<head>';
+	h += '<title>Watcha Watching</title>';
+	h += '<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>';
+	h += '<link href="/css-norm" type="text/css" rel="stylesheet" />';
+	h += '<link href="/css-base" type="text/css" rel="stylesheet" />';
+	h += '</head>';
+
+	callback(null, h);
+}
+
+function loadNav(callback) {
 	video.loadCollection(function(err, videos) {
 		if (err) {
 			throw err;
 		}
 
 		if (videos.length) {
+			var nav = '';
 			for (var i = 0; v = videos[i], i < videos.length; i++) {
-				res.write('<p><a href="/' + v.source + '">' + v.name + '</a></p>');
-			};
+				nav += '<li><a href="/' + v.source.toLowerCase() + '">' + v.name + '</a></li>';
+			}
+
+			callback(null, '<div id="side"><ul>' + nav + '</ul><br clear="all" /></div>');
 		} else {
-			throw new Error('no videos were returned');
+			callback(new Error('no videos were returned'));
 		}
-		res.end();
-	});				
+	});		
+}
+
+app.get('/youtube', function(req, res){
+	page_load(req, res, 1);
 });
 
-app.get('/css', function(req, res){
-	// this is only a test. Dont be alarmed!!!
+app.get('/dailymotion', function(req, res){
+	page_load(req, res, 3);
+});
+
+app.get('/vimeo', function(req, res){
+	page_load(req, res, 2);
+});
+
+app.get('/bliptv', function(req, res){
+	page_load(req, res, 4);		
+});
+
+app.get('/css-norm', function(req, res){
 	res.header('Content-Type', 'text/css');
 	var rs = fs.createReadStream(__dirname + '/public/css/normalize.css');
 	util.pump(rs, res);	
 });
 
-app.get('/youtube', function(req, res){
-	video.load(1, function(err, vid) {
-			videoEmbedHtml.getEmbedHtml(vid.url, vid.source.toLowerCase(), function(err, embedHtml) {
-				if (err) {
-					throw err;
-				}
-
-				res.header('Content-Type', 'text/html');	
-				res.write(embedHtml);
-				res.end();					
-			});
-	});
+app.get('/css-base', function(req, res){
+	res.header('Content-Type', 'text/css');
+	var rs = fs.createReadStream(__dirname + '/public/css/base.css');
+	util.pump(rs, res);	
 });
 
-app.get('/dailymotion', function(req, res){
-	video.load(3, function(err, vid) {
-			videoEmbedHtml.getEmbedHtml(vid.url, vid.source.toLowerCase(), function(err, embedHtml) {
-				if (err) {
-					throw err;
-				}
-
-				res.header('Content-Type', 'text/html');	
-				res.write(embedHtml);
-				res.end();					
-			});
-	});
+app.get('/', function(req, res){
+	res.header('Content-Type', 'text/html');
+	res.write('Watcha Watching');
+	res.end();
 });
-
-app.get('/vimeo', function(req, res){
-	video.load(2, function(err, vid) {
-			videoEmbedHtml.getEmbedHtml(vid.url, vid.source.toLowerCase(), function(err, embedHtml) {
-				if (err) {
-					throw err;
-				}
-
-				res.header('Content-Type', 'text/html');	
-				res.write(embedHtml);
-				res.end();					
-			});
-	});
-});
-
-app.get('/bliptv', function(req, res){
-	video.load(4, function(err, vid) {
-			videoEmbedHtml.getEmbedHtml(vid.url, vid.source.toLowerCase(), function(err, embedHtml) {
-				if (err) {
-					throw err;
-				}
-
-				res.header('Content-Type', 'text/html');	
-				res.write(embedHtml);
-				res.end();					
-			});
-	});
-
-	// res.header('Content-Type', 'text/html');	
-	// res.write('<iframe src="http://blip.tv/play/gbk7gufWHwI.html?p=1" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>');
-	// res.end();					
-});
-
-
-// app.get('/pump', function(req, res){
-// 	res.header('Content-Type', 'text/html');	
-// 	var rs = fs.createReadStream(__dirname + '/public/html_templates/index.html');
-// 	console.log(__dirname + '/public/html_templates/index.html');
-// 	util.pump(rs, res);
-// 	res.end('hello');
-// });
 
 app.listen(1979);
 
